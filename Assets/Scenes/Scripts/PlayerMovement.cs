10da09cb3729deddb45c426f5c305b71ad6496bf
Ondrej Cerny <sprotex@gmail.com>
2020-10-03 11:37:04 +0200

    add basic movement

---
+++ Assets/Scenes/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cameraTransform;
+    public float maxMoveSpeed;
+
+    private void Update()
+    {
+        Translate();
+        Rotate();
+    }
+
+    private void Translate()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        var movementForward = Vector3.Cross(cameraTransform.right, Vector3.up);
+        var movementRight = cameraTransform.right;
+        var motionVector = movementForward * vertical + movementRight * horizontal;
+        if (motionVector.sqrMagnitude > 1f)
+            motionVector.Normalize();
+        controller.Move(motionVector * maxMoveSpeed * Time.deltaTime);
+    }
+
+    private void Rotate()
+    {
+        
+    }
+}
---
+++ Assets/Scenes/Scripts/Tasks/Task.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Task : MonoBehaviour
+{
+    public void TaskSuccess()
+    {
+        print("Task: Success!");
+    }
+
+    public void TaskFailure()
+    {
+        print("Task: Failed!");
+    }
+}
