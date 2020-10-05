5dd5417dcd755345418a7fda72ce06ac54c67a03
Ondrej Cerny <sprotex@gmail.com>
2020-10-05 10:55:40 +0200

    add sensitivity
    add credits

---
+++ Assets/CinemachineSensitivitySettings.cs
@@ -0,0 +1,19 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinemachineSensitivitySettings : MonoBehaviour
+{
+    public CinemachineVirtualCamera cmCamera;
+    
+    private void Start()
+    {
+        var sensitivity = PlayerPrefs.GetFloat(Settings.actualSensitivityKey, 1f);
+        var maxSpeed = sensitivity * 300;
+        var pov = cmCamera.GetCinemachineComponent<CinemachinePOV>();
+        print(sensitivity);
+        pov.m_HorizontalAxis.m_MaxSpeed = maxSpeed;
+        pov.m_VerticalAxis.m_MaxSpeed = maxSpeed;
+    }
+}
---
+++ Assets/Settings.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public const string sensitivityKey = "sensitivity";
+    public const string actualSensitivityKey = "actual sensitivity";
+    public const string volumeKey = "volume";
+    private float volume;
+    private float sensitivity;
+
+    public Slider volumeSlider;
+    public Slider sensitivitySlider;
+
+    public void UpdateSettings()
+    {
+        PlayerPrefs.SetFloat(volumeKey, volumeSlider.value);
+        PlayerPrefs.SetFloat(sensitivityKey, sensitivitySlider.value);
+        var actualSensitivity = 1f;
+        if (sensitivitySlider.value >= 4f)
+        {
+            var t = Mathf.InverseLerp(4f, 8f, sensitivitySlider.value);
+            actualSensitivity = Mathf.Lerp(1f, 4f, t);
+        }
+        if (sensitivitySlider.value < 4f)
+        {
+            var t = Mathf.InverseLerp(0f, 4f, sensitivitySlider.value);
+            actualSensitivity = Mathf.Lerp(1 / 8f, 1f, t);
+        }
+        PlayerPrefs.SetFloat(actualSensitivityKey, actualSensitivity);
+    }
+
+    void Start()
+    {
+        volume = PlayerPrefs.GetFloat(volumeKey, 0.5f);
+        sensitivity = PlayerPrefs.GetFloat(sensitivityKey, 4f);
+        if (volumeSlider != null)
+            volumeSlider.value = volume;
+        if (sensitivitySlider != null)
+            sensitivitySlider.value = sensitivity;
+    }
+}
