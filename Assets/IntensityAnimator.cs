a384afb7a1a7293666581dff6ac069539d9d7052
Ondrej Cerny <sprotex@gmail.com>
2020-10-05 14:10:46 +0200

    add pc light intensity animator

---
+++ Assets/IntensityAnimator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntensityAnimator : MonoBehaviour
+{
+    public Light animatedLight;
+    private float maxIntensity;
+    private float offset = 1f;
+
+    private void Start() => maxIntensity = animatedLight.intensity;
+    private void Update() => animatedLight.intensity = maxIntensity - offset + offset * Mathf.PerlinNoise(0f, Time.time * 5f);
+}
