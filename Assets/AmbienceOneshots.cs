c3e575691e53df876dd77fb06650ee5fa29faa6e
Ondrej Cerny <sprotex@gmail.com>
2020-10-05 12:26:48 +0200

    bug ambient sounds

---
+++ Assets/AmbienceOneshots.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AmbienceOneshots : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string[] soundEvents;
+    public Transform[] positions;
+    public Vector2 randomTimeOffset;
+
+    private IEnumerator PlayOnRandomLocation()
+    {
+        while(true)
+        {
+            var waitTime = Random.Range(randomTimeOffset.x, randomTimeOffset.y);
+            yield return new WaitForSeconds(waitTime);
+            var position = positions[Random.Range(0, positions.Length)];
+            var soundEvent = soundEvents[Random.Range(0, soundEvents.Length)];
+            var instance = FMODUnity.RuntimeManager.CreateInstance(soundEvent);
+            instance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(position));
+            instance.start();
+            instance.release();
+        }
+    }
+
+    private void OnEnable() => StartCoroutine(PlayOnRandomLocation());
+    private void OnDisable() => StopAllCoroutines();
+
+}
